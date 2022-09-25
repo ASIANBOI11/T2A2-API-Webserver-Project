0aa0dbb2ac72099bd60acfb2df05a8b8715a5b70
ASIANBOI11 <tommy.truong962@gmail.com>
2022-09-26 00:27:30 +1000

    remove pycache from .gitignore

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
+db = SQLAlchemy()
+ma = Marshmallow()
+bcrypt = Bcrypt()
+jwt = JWTManager()
+
 
 def create_app():
     app = Flask(__name__)
